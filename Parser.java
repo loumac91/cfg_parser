9b55f196670bc969c9ed5996803e33b708698561
loumac91 <loumac91@googlemail.com>
2020-11-18 18:19:07 +0000

    with linkedlist

--- Parser.java
+++ Parser.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,56 +18,59 @@ public class Parser implements IParser {
     // all derivatins with 1 step
     // If any of these derivations generate w, then accept. Otherwise reject
 
+    // Algorithm #1
+    // Iterate through each set of expansions, computing all possible derivations
+    // i = 0, [ S ]
+    // i = 1, [ EG | TH | NC | 1 | 0 | x ]
+    // i = 2, [ EEG, EPT | TTH, TMF | -C, N1, N0, Nx ]
+    // i = 3, [ EGEG, EEGG, EEPT ... etc ]
+
     List<Rule> rules = cfg.getRules();
     Map<Symbol, List<Rule>> ruleMap = rules
       .stream()
       .collect(Collectors.groupingBy(Rule::getVariable));
 
-    // i = 0, [ S ]
-    // i = 1, [ EG | TH | NC | 1 | 0 | x ]
-    // i = 2, [ EEG, EPT | TTH, TMF | -C, N1, N0, Nx ]
-    // i = 3, [ EGEG, EEGG, EEPT | EG | ]
-
-    // Start at start variable
-    // i = 0,
-
-    // derivations of x+x
-    // S => EG => xG => xPT => x+T => x+x
+    // Ignore whitespace
     Word cleaned = new Word(w.toString().replaceAll("\\s",""));
     int n = cleaned.length();
     int derivations = 2 * n - 1;
 
     Variable current = cfg.getStartVariable();
-    ArrayList<Word> previousExpansions = new ArrayList<>(){{
-      add(new Word(current.toString())); 
+    LinkedList<Word> previousExpansions = new LinkedList<>(){{
+      push(new Word(current.toString())); 
     }};
 
     int i = 1;
-    while (i <= derivations) {
-      
+    while (i <= derivations) { // On my computer, things really start to grind to a halt after the 10th iteration, and we start running into memory issues
+      LinkedList<Word> currentExpansions = new LinkedList<>();
+      // Iterate through each previous expansions
+      while (true) {
+        Word word = previousExpansions.peek();
+        if (word == null) break;
 
-      ArrayList<Word> currentExpansions = new ArrayList<>();
-      // Get all of the derivations of the words
-      for (Word word : previousExpansions) {
+        word = previousExpansions.pop();
+        // If the whole derivation is a word, then go to next derivation
         if (word.isTerminal()) {
           continue;
         }
 
-        // then there are derivations
-        // iterate through each symbol, and get the unique set of derivations
+        // Iterate through each symbol of the derivation
         int replaceIndex = 0;
         Iterator<Symbol> symbols = word.iterator();
         while (symbols.hasNext()) { 
           Symbol currentSymbol = symbols.next();
+          // If the symbol is a terminal, then skip
           if (currentSymbol.isTerminal()) {
             replaceIndex++;
             continue;
           }
+
+          // Get all the expansions for the symbol and apply them
           List<Rule> currentRules = ruleMap.getOrDefault(currentSymbol, new ArrayList<Rule>());
           for (Rule r : currentRules) {
-            // replace current symbol with expansion
             Word replaced = word.replace(replaceIndex, r.getExpansion());
-            currentExpansions.add(replaced);
+            if (replaced.equals(cleaned)) return true; // If we get out result, return early
+            currentExpansions.push(replaced);
           }
 
           replaceIndex++;
@@ -76,8 +80,8 @@ public class Parser implements IParser {
       previousExpansions = currentExpansions;
       i++;
     }
-    return previousExpansions
-      .contains(cleaned);
+
+    return false;
   }
 
   public ParseTreeNode generateParseTree(ContextFreeGrammar cfg, Word w) {
